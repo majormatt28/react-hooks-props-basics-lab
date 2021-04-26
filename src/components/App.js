474a10e0a1ebe6c19ff996840f3eefd8fccf7c57
Matthew <elijah2895@hotmail.com>
2021-04-26 16:56:26 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -11,8 +11,8 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home color="firebrick" username="Liza" city="New York"/>
+      <About bio={user.bio} links={user.links} />
     </div>
   );
 }
